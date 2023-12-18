3fb84acafaf0c84c72605470c22b38aa2dd082f0
rihu1424 <rihu1424@colorado.edu>
2023-12-17 19:59:00 -0800

    final final

--- js-final/jsfinal.js
+++ js-final/jsfinal.js
@@ -5,13 +5,19 @@ let lastLength = 0;
 let formatInput = '';
 
 phoneInput.addEventListener('keydown', (e) => {
+    // Check for spacebar key press
+    if (e.key === ' ') {
+        // also display an annoying pop-up when spacebar is hit
+        alert('This is going to happen every time, you have no control over this or the numbers.');
+    }
+
     // no letters, just numbers
-    if (e.key !== ' ') {
+    if (e.key !== ' ' || formatInput.length >= 10) {
         e.preventDefault();
         return;
     }
 
-    // make random digit for new number input
+    // make random digit for new input
     formatInput += Math.floor(Math.random() * 10);
 
     // add hyphens for xxx-xxx-xxxx format
@@ -26,13 +32,13 @@ resetButton.addEventListener('click', () => {
     lastLength = 0;
 
     //let the user know to try again
-    document.getElementById('message').textContent = 'Try Again';
+    document.getElementById('message').textContent = 'Oh nooooooo! Try Again';
 
 });
 
 enterButton.addEventListener('click', function() {
 
     //display congrats when yes is clicked
-    document.getElementById('message').textContent = 'Congratulations!';
+    document.getElementById('message').textContent = 'Not sure I believe you, but congratulations!';
 
 });
